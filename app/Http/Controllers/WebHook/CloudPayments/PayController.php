50f3a7dcb5f2cf67c1e346396970d4a4024e3be8
neoko <33611354+neokofg@users.noreply.github.com>
2024-04-06 17:47:29 +0900

    feat/return

--- app/Http/Controllers/WebHook/CloudPayments/PayController.php
+++ app/Http/Controllers/WebHook/CloudPayments/PayController.php
@@ -5,12 +5,19 @@ namespace App\Http\Controllers\WebHook\CloudPayments;
 use App\Http\Controllers\Controller;
 use App\Jobs\SendData;
 use App\Models\User;
+use App\Presenters\JsonPresenter;
 use Faker\Provider\ru_RU\Person;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 
 class PayController extends Controller
 {
+    public function __construct(
+        private JsonPresenter $presenter
+    )
+    {
+    }
+
     public function __invoke(Request $request)
     {
         $password = rand(100000,999999);
@@ -21,5 +28,6 @@ class PayController extends Controller
             'password' => Hash::make($password),
         ]);
         SendData::dispatch($email,$password)->onQueue('default');
+        return $this->presenter->present(['code' => 0]);
     }
 }
