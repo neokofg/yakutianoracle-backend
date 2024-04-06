de6e0b9cb5973ad90e3c3323773c01bcbb9a3d55
neoko <33611354+neokofg@users.noreply.github.com>
2024-04-07 08:17:15 +0900

    fix/cors

--- config/cors.php
+++ config/cors.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie', 'territory.geojson'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie', 'territory.geojson', '*'],
 
     'allowed_methods' => ['*'],
 
